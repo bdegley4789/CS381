7f2410b463d39007b0000499991d431440419faf
bdegley4789 <egleyb@oregonstate.edu>
2018-01-29 12:01:54 -0800

    Minilogo start

---
+++ MiniLogo.egleyb.hs
@@ -0,0 +1,175 @@
+-- | Team Members: Bryce Egley ONID: egleyb, Kenneth Price ONID: pricek, Kenneth Thompson ONID: thomkenn
+module Nat where
+
+import Prelude hiding (Enum(..), sum)
+
+
+--
+-- * Part 2: Natural numbers
+--
+
+-- | The natural numbers.
+data Nat = Zero
+         | Succ Nat
+         deriving (Eq,Show)
+
+-- | The number 1.
+one :: Nat
+one = Succ Zero
+
+-- | The number 2.
+two :: Nat
+two = Succ one
+
+-- | The number 3.
+three :: Nat
+three = Succ two
+
+-- | The number 4.
+four :: Nat
+four = Succ three
+
+
+-- | The predecessor of a natural number.
+--
+--   >>> pred Zero
+--   Zero
+--
+--   >>> pred three
+--   Succ (Succ Zero)
+--
+pred :: Nat -> Nat
+pred Zero = Zero
+pred (Succ n) = n
+
+
+
+
+-- | True if the given value is zero.
+--
+--   >>> isZero Zero
+--   True
+--
+--   >>> isZero two
+--   False
+--
+isZero :: Nat -> Bool
+isZero Zero = True
+isZero (n) = False
+
+
+-- | Convert a natural number to an integer.
+--
+--   >>> toInt Zero
+--   0
+--
+--   >>> toInt three
+--   3
+--
+toInt :: Nat -> Int
+toInt Zero = 0
+toInt (Succ n) = 1 + toInt n
+
+
+-- | Add two natural numbers.
+--
+--   >>> add one two
+--   Succ (Succ (Succ Zero))
+--
+--   >>> add Zero one == one
+--   True
+--
+--   >>> add two two == four
+--   True
+--
+--   >>> add two three == add three two
+--   True
+--
+add :: Nat -> Nat -> Nat
+add Zero n2 = n2
+add n1 n2 = add (pred n1) (Succ n2)
+
+
+-- | Subtract the second natural number from the first. Return zero
+--   if the second number is bigger.
+--
+--   >>> sub two one
+--   Succ Zero
+--
+--   >>> sub three one
+--   Succ (Succ Zero)
+--
+--   >>> sub one one
+--   Zero
+--
+--   >>> sub one three
+--   Zero
+--
+sub :: Nat -> Nat -> Nat
+sub n2 Zero = Zero
+sub Zero n1 = n1
+sub n2 n1 = sub (pred n1) (pred n2)
+
+
+-- | Is the left value greater than the right?
+--
+--   >>> gt one two
+--   False
+--
+--   >>> gt two one
+--   True
+--
+--   >>> gt two two
+--   False
+--
+gt :: Nat -> Nat -> Bool
+gt n2 n1 = not (sub n2 n1 == Zero)
+
+
+-- | Multiply two natural numbers.
+--
+--   >>> mult two Zero
+--   Zero
+--
+--   >>> mult Zero three
+--   Zero
+--
+--   >>> toInt (mult two three)
+--   6
+--
+--   >>> toInt (mult three three)
+--   9
+--
+mult :: Nat -> Nat -> Nat
+mult Zero _ = Zero
+mult _ Zero = Zero
+mult (Succ Zero) n2 = n2
+mult n1 n2 = add n2 (mult (pred n1) n2)
+
+
+-- | Compute the sum of a list of natural numbers.
+--
+--   >>> sum []
+--   Zero
+--
+--   >>> sum [one,Zero,two]
+--   Succ (Succ (Succ Zero))
+--
+--   >>> toInt (sum [one,two,three])
+--   6
+--
+sum :: [Nat] -> Nat
+sum [] = Zero
+sum (i:t) = add i (sum t)
+
+
+-- | An infinite list of all of the *odd* natural numbers, in order.
+--
+--   >>> map toInt (take 5 odds)
+--   [1,3,5,7,9]
+--
+--   >>> toInt (sum (take 100 odds))
+--   10000
+--
+odds :: [Nat]
+odds = one : map (add two) odds
