d62d093ffa368901d41907449f99654ca1056f30
pricek <pricek@oregonstate.edu>
2018-01-29 13:03:15 -0800

    steps and macros

--- MiniLogo.egleyb.hs
+++ MiniLogo.egleyb.hs
@@ -21,3 +21,16 @@ data Cmd = Pen Mode
          | Move Expr Expr
          | Define Macro [Var] Prog
          | Call Macro [Expr]
+
+
+singleStep :: Int -> Prog
+singleStep xy = [Call line [LitN xy+1, LitN xy+1, LitN xy, LitN xy+1], Call line [LitN xy, LitN xy, LitN xy, LitN xy+1] 
+         
+steps :: Int -> Prog
+steps x =  steps x-1 ++ singleStep x
+
+macros :: Prog -> [Macro]
+macros cmd : prog = case cmd of
+                    Define m _ _ -> m : (macros prog)
+                    _ -> macros prog
+                    
