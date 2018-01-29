87e3fc122d50b974601cd3ca8f16c125aa756144
bdegley4789 <egleyb@oregonstate.edu>
2018-01-29 12:15:15 -0800

    Prelude

--- MiniLogo.egleyb.hs
+++ MiniLogo.egleyb.hs
@@ -2,3 +2,22 @@
 module MiniLogo where
 
 import Prelude hiding (Num)
+
+type Num Int
+
+type Var String
+
+type Macro String
+
+type Prog [Cmd]
+
+data Mode = Down | Up
+
+data Expr = LitV Var
+          | LitN Num
+          | Add Expr Expr
+
+dara Cmd = Pen Mode
+         | Move Expr Expr
+         | Define Macro [Var] Prog
+         | Call Macro [Expr]
